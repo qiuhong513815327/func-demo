488b4b98adb7a4022ed029619f01df44c3b9f8da
qiuhong513815327 <www513815327@qq.com>
2021-03-31 16:55:02 +0800

    immg和link的跨域请求实现

---
+++ 跨域/demo1.js
@@ -0,0 +1,29 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const fs = require("fs");
+const path = require("path");
+
+const imgPath = path.join(__dirname + "/imgs/0.jpg");
+console.log(path.join("__dirname" + "./imgs/0.jpg"));
+const img = fs.readFileSync(imgPath);
+const app = new Koa();
+const router = new Router();
+
+router.get("/getData", (ctx, next) => {
+  console.log("收到请求");
+  ctx.body = { name: "get" };
+  next();
+});
+router.get("/getImg", (ctx, next) => {
+  console.log("收到图片请求");
+  ctx.body = img;
+  next();
+});
+router.post("/postData", (ctx, next) => {
+  ctx.body = { name: "post" };
+  next();
+});
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(9000, () => {
+  console.log("服务启动:::9000");
+});
